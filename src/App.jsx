abef9cef6e47b42fc68638e617ca7a48f78ed767
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2023-10-06 01:21:43 +0200

    Add basic formik example

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { object, string, number, bool } from "yup";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //state management
+  const initialValues = {
+    name: "",
+    email: "",
+    phone: "",
+    age: "",
+    select: "",
+    checkbox: "",
+  };
+
+  // validation schema
+  const phoneRegExp =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+  const validationSchema = object({
+    name: string().required("your name is required").min(2).max(15),
+    email: string().required("your email is required").email("your email is invalid"),
+    phone: string()
+      .required("youre phone number is required")
+      .matches(phoneRegExp, "phone number is not valid")
+      .min(10, "too short")
+      .max(10, "too long"),
+    age: number()
+      .required("your age is required")
+      .positive("age can not be negative")
+      .integer("your age needs to be a integer"),
+    select: string().required("option is required").oneOf(["option1", "option2", "option3"]),
+    checkbox: bool().required("checkbox needs to be checked").oneOf([true], "checkbox needs to be checked"),
+  });
+
+  //submithandler
+  const handleSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div>
+            <label htmlFor="name">name</label>
+            <Field
+              name="name"
+              id="name"
+            />
+            <ErrorMessage
+              name="name"
+              component="p"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email">email</label>
+            <Field
+              name="email"
+              id="email"
+            />
+            <ErrorMessage
+              name="email"
+              component="p"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="phone">phone</label>
+            <Field
+              name="phone"
+              id="phone"
+            />
+            <ErrorMessage
+              name="phone"
+              component="p"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="age">age</label>
+            <Field
+              name="age"
+              id="age"
+            />
+            <ErrorMessage
+              name="age"
+              component="p"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="select">select</label>
+            <Field
+              name="select"
+              as="select"
+              id="select"
+            >
+              <option value="">Choose an option</option>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Field>
+            <ErrorMessage
+              name="select"
+              component="p"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="checkbox">checkbox</label>
+            <Field
+              id="checkbox"
+              name="checkbox"
+              type="checkbox"
+            />
+            <ErrorMessage
+              name="checkbox"
+              component="p"
+            />
+          </div>
+
+          <button type="submit">submit</button>
+        </Form>
+      </Formik>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
